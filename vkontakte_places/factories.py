4bf8fdd888d224487cd3da042bb3e7523448e153
ramusus <ramusus@gmail.com>
2013-05-09 16:49:53 +0400

    updated factories to 2.0

--- vkontakte_places/factories.py
+++ vkontakte_places/factories.py
@@ -1,12 +1,12 @@
 from models import City, Country
 import factory
 
-class CityFactory(factory.Factory):
+class CityFactory(factory.DjangoModelFactory):
     FACTORY_FOR = City
 
     remote_id = factory.Sequence(lambda n: n)
 
-class CountryFactory(factory.Factory):
+class CountryFactory(factory.DjangoModelFactory):
     FACTORY_FOR = Country
 
     remote_id = factory.Sequence(lambda n: n)
